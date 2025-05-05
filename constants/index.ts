6978c16436d77ca38ce28ab1552457755abe3ca7
Jozef S <franck.vukelic@gmail.com>
2025-05-05 11:42:34 +0700

    Update CSS and Constants

--- constants/index.ts
+++ constants/index.ts
@@ -1,12 +1,13 @@
 export const MAX_VIDEO_SIZE = 500 * 1024 * 1024;
 export const MAX_THUMBNAIL_SIZE = 10 * 1024 * 1024;
 
-export const BUNNY_STREAM_BASE_URL = "https://video.bunnycdn.com/library";
-export const BUNNY_STORAGE_BASE_URL =
-  "https://sg.storage.bunnycdn.com/snapcast";
-export const BUNNY_CDN_URL = "https://snapcast.b-cdn.net";
-export const BUNNY_EMBED_URL = "https://iframe.mediadelivery.net/embed";
-export const BUNNY_TRANSCRIPT_URL = "https://vz-47a08e64-84d.b-cdn.net";
+export const BUNNY = {
+  STREAM_BASE_URL: "https://video.bunnycdn.com/library",
+  STORAGE_BASE_URL: "https://sg.storage.bunnycdn.com/snapcast",
+  CDN_URL: "https://snapcast.b-cdn.net",
+  EMBED_URL: "https://iframe.mediadelivery.net/embed",
+  TRANSCRIPT_URL: "https://vz-47a08e64-84d.b-cdn.net",
+};
 
 export const emojis = ["😂", "😍", "👍"];
 
@@ -32,10 +33,16 @@ export const initialVideoState = {
   processingProgress: 0,
 };
 
-export const reactionCounts = {
-  "😂": 0,
-  "😍": 0,
-  "👍": 0,
+export const infos = ["transcript", "metadata"];
+
+export const DEFAULT_VIDEO_CONFIG = {
+  width: { ideal: 1920 },
+  height: { ideal: 1080 },
+  frameRate: { ideal: 30 },
 };
 
-export const infos = ["transcript", "metadata"];
+export const DEFAULT_RECORDING_CONFIG = {
+  mimeType: "video/webm;codecs=vp9,opus",
+  audioBitsPerSecond: 128000,
+  videoBitsPerSecond: 2500000,
+};
