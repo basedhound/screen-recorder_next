1a6bf191c92173a783557b12e01e8e5f40ce3ed7
Jozef S <franck.vukelic@gmail.com>
2025-05-06 11:42:47 +0700

    Add video schema

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -4,6 +4,8 @@ import {
   timestamp,
   boolean,
   integer,
+  uuid,
+  jsonb,
 } from "drizzle-orm/pg-core";
 
 export const user = pgTable("user", {
@@ -54,6 +56,26 @@ export const verification = pgTable("verification", {
   expiresAt: timestamp("expires_at").notNull(),
   createdAt: timestamp("created_at"),
   updatedAt: timestamp("updated_at"),
+});
+
+export const videos = pgTable("videos", {
+  id: uuid("id").primaryKey().defaultRandom().unique(),
+  title: text("title").notNull(),
+  description: text("description").notNull(),
+  videoUrl: text("video_url").notNull(),
+  videoId: text("video_id").notNull(),
+  thumbnailUrl: text("thumbnail_url").notNull(),
+  visibility: text("visibility").$type<"public" | "private">().notNull(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+  views: integer("views").notNull().default(0),
+  reactions: jsonb("reactions")
+    .default([])
+    .$type<Array<{ userId: string; emoji: string }>>(),
+  duration: integer("duration"),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
 
 export const schema = {
