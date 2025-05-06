e9cce0b0fee0edd251f00f5213c4761976c7ead2
Jozef S <franck.vukelic@gmail.com>
2025-05-06 11:34:47 +0700

    Add File Upload page, form, inputs

---
+++ app/(root)/upload/page.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import { useState, FormEvent, ChangeEvent, useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+import { FileInput, FormField } from "@/components";
+
+import { useFileInput } from "@/lib/hooks/useFileInput";
+import { MAX_THUMBNAIL_SIZE, MAX_VIDEO_SIZE } from "@/constants";
+
+const UploadPage = () => {
+  const router = useRouter();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [formData, setFormData] = useState<VideoFormValues>({
+    title: "",
+    description: "",
+    tags: "",
+    visibility: "public",
+  });
+
+  const video = useFileInput(MAX_VIDEO_SIZE);
+  const thumbnail = useFileInput(MAX_THUMBNAIL_SIZE);
+
+  const handleInputChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const onSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    console.log(e);
+  };
+
+  return (
+    <main className="wrapper-md upload-page">
+      <h1>Upload a video</h1>
+      {error && <div className="error-field">{error}</div>}
+      <form
+        className="rounded-20 gap-6 w-full flex flex-col shadow-10 px-5 py-7.5"
+        onSubmit={onSubmit}>
+        <FormField
+          id="title"
+          label="Title"
+          value={formData.title}
+          onChange={handleInputChange}
+          placeholder="Enter a clear and concise video title"
+        />
+
+        <FormField
+          id="description"
+          label="Description"
+          value={formData.description}
+          onChange={handleInputChange}
+          placeholder="Briefly describe what this video is about"
+          as="textarea"
+        />
+        <FormField
+          id="visibility"
+          label="Visibility"
+          value={formData.visibility}
+          onChange={handleInputChange}
+          as="select"
+          options={[
+            { value: "public", label: "Public" },
+            { value: "private", label: "Private" },
+          ]}
+        />
+
+        <FileInput
+          id="video"
+          label="Video"
+          accept="video/*"
+          file={video.file}
+          previewUrl={video.previewUrl}
+          inputRef={video.inputRef}
+          onChange={video.handleFileChange}
+          onReset={video.resetFile}
+          type="video"
+        />
+
+        <FileInput
+          id="thumbnail"
+          label="Thumbnail"
+          accept="image/*"
+          file={thumbnail.file}
+          previewUrl={thumbnail.previewUrl}
+          inputRef={thumbnail.inputRef}
+          onChange={thumbnail.handleFileChange}
+          onReset={thumbnail.resetFile}
+          type="image"
+        />
+
+        <button type="submit" disabled={isSubmitting} className="submit-button">
+          {isSubmitting ? "Uploading..." : "Upload Video"}
+        </button>
+      </form>
+    </main>
+  );
+};
+
+export default UploadPage;
--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,5 @@ export { default as Navbar } from "./Navbar";
 export { default as ImageWithFallback } from "./ImageWithFallback";
 export { default as SharedHeader } from "./SharedHeader";
 export { default as VideoCard } from "./VideoCard";
+export { default as FormField } from "./FormField";
+export { default as FileInput } from "./FileInput";
